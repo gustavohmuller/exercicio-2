8225159b846af1eaccd59ec3f9416451f90cd8dd
gustavohmuller <99853045+gustavohmuller@users.noreply.github.com>
2022-02-17 23:13:19 -0300

    Update Consultas.sql

--- Consultas.sql
+++ Consultas.sql
@@ -8,8 +8,8 @@ Order by dataInfracao;
 -- 2. Monte uma busca que para cada remessa apresente sua identificação, sua situação, o total de infrações, total de infrações válidas e total de infrações inválidas. Ordenar a consulta pela situação das remessas.
 
     Select idRemessa as 'Identificação Numérica', situacao as 'Situação', totalInfracoes as 'Total de infrações', Count(case when valorBinario = 1 then valorBinario end) as 'Total de infrações válidas', Count(case when valorBinario = 0 then valorBinario end) as 'Total de infrações inválidas'
-	  From remessa
+      From remessa
 Inner join infracao
-		On idRemessa = Remessa_idRemessa
+	On idRemessa = Remessa_idRemessa
   Group by idRemessa
   Order by situacao;
